f8031dd86f77a568ce70fd6ff59fbe7a13d6b77a
Mennagamal272 <menagmal272@gmail.com>
2024-05-05 20:40:25 +0200

    task2

---
+++ Task_2/ros_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/menna/ros_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/menna/ros_ws/devel;/home/menna/ras_ws/devel;/home/menna/first_ws/devel;/home/menna/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/menna/ros_ws/devel;/home/menna/ras_ws/devel;/home/menna/first_ws/devel;/home/menna/catkin_ws/devel;/opt/ros/noetic' != '' else []
---
+++ Task_2/ros_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export PATH='/opt/ros/noetic/bin:/path/to/python/directory:/home/menna/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
+export PWD='/home/menna/ros_ws/build'
